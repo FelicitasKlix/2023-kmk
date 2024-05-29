f3f2e1d7086a0452d73db86c485cc8ead10ca825
FelicitasKlix <felikpc@gmail>
2024-05-29 20:08:06 -0300

    start services

---
+++ start_services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start the notifications API service
+cd pid-notifications-api
+poetry run start
+NOTIFICATIONS_API_PID=$!
+
+# Wait for the notifications API to be ready
+echo "Waiting for notifications API to start..."
+sleep 20  # Ajusta el tiempo de espera según sea necesario
+
+# Start the main backend service
+cd ../pid-be
+poetry run start
+
+# Wait for both services to finish
+wait $NOTIFICATIONS_API_PID
